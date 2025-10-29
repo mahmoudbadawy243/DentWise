0e5623c51ebbb0e2ddbcf2f5b5e19c01f2618dae
Your Name <mahmoudbadawy243@gmail.com>
2025-10-29 17:28:52 +0300

    Build WhatToAsk UI

--- src/components/landing/HowItWorks.tsx
+++ src/components/landing/HowItWorks.tsx
@@ -5,9 +5,9 @@ import { Button } from "../ui/button";
 
 function HowItWorks() {
   return (
-    <section className="relative py-32 px-6 z-10 max-w-7xl mx-auto">
+    <section className="relative pt-32 pb-10 px-6 z-10 max-w-7xl mx-auto">
       {/* HEADER */}
-      <div className="text-center mb-20">
+      <div className="text-center mb-20 space-y-10">
         <div className="inline-flex items-center gap-2 px-4 py-2 bg-linear-to-r from-primary/5 to-primary/10 rounded-full border border-primary/10 backdrop-blur-sm mb-6">
           <ZapIcon className="size-4 text-primary" />
           <span className="text-sm font-medium text-primary">Simple Process</span>
