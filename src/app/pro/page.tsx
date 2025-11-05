7b23c70e0f10a6b63ae219d4f8acc289ddf6a685
Your Name <mahmoudbadawy243@gmail.com>
2025-11-05 14:07:49 +0200

    Create PRO page and Pricing tables

---
+++ src/app/pro/page.tsx
@@ -0,0 +1,65 @@
+import Navbar from "@/components/Navbar";
+import { PricingTable } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { CrownIcon } from "lucide-react";
+import { redirect } from "next/navigation";
+// import { neobrutalism } from '@clerk/themes'
+
+
+
+async function ProPage() {
+
+  const user = await currentUser();
+  if (!user) redirect("/");
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <div className="mb-12 pt-2 overflow-hidden">
+          <div className="flex items-center justify-between bg-linear-to-br from-primary/10 to-background rounded-3xl p-8 border border-primary/20">
+            <div className="space-y-4">
+              <div className="inline-flex items-center gap-2 px-3 py-1 bg-primar/10 rounded-full border border-primary/20 ">
+                <div className="w-2 h-2 bg-primary rounded-full animate-pulse"></div>
+                <span className="text-sm font-medium text-primary">Upgrade to Pro</span>
+              </div>
+
+              <div>
+                <h1 className="text-4xl font-bold mb-2">Unlock Premium AI Dental Care</h1>
+                <p className="text-muted-foreground max-w-4xl">
+                  Get unlimited AI consultations, advanced features, and priority support to take
+                  your dental health to the next level.
+                </p>
+              </div>
+            </div>
+
+            <div className="hidden lg:block">
+              <div className="w-32 h-32 bg-linear-to-br from-primary/20 to-primary/10 rounded-full flex items-center justify-center">
+                <CrownIcon className="w-16 h-16 text-primary" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* PRICING SECTION */}
+        <div className="space-y-8 pb-20">
+          <div className="text-center space-y-4 pb-10">
+            <h2 className="text-3xl font-bold">Choose Your Plan</h2>
+            <p className="text-muted-foreground max-w-2xl mx-auto">
+              Select the perfect plan for your dental care needs. All plans include secure access
+              and bank-level encryption.
+            </p>
+          </div>
+
+          {/* this is RESERVED name that is imported from clerk depend on plans that i created on clerk site */}
+          <PricingTable 
+          appearance={{ variables: { colorPrimary: "oklch(0.6716 0.1368 48.5130)",} }} />
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProPage;
